b921623724dbff596697d603207751a41ca12fdf
Moayed Alhagy <moayedlaravel@gmail.com>
2024-09-30 01:20:14 +0300

    ok is work now !

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { Button, MantineProvider } from "@mantine/core";
+
 function App() {
   return (
-    <>
+    <MantineProvider>
       <p className="bg-red-600">
         Lorem ipsum, dolor sit amet consectetur adipisicing elit.
       </p>
-    </>
+      <Button color="green"> click me</Button>
+    </MantineProvider>
   );
 }
 
