fc4b5d223a94e2bb8c84ac10e675a8d3bdaaea79
Moayed Alhagy <moayedlaravel@gmail.com>
2024-11-03 13:07:43 +0300

    Who us item

--- src/components/PagesMangerItems/WhoUsItem.tsx
+++ src/components/PagesMangerItems/WhoUsItem.tsx
@@ -1,10 +1,51 @@
+import { RiGalleryLine } from "@remixicon/react";
 import AccordionItem from "../AccordionItem";
+import ButtonIcon from "../ButtonIcon";
+import { Image } from "@mantine/core";
 
 export default function WhoUsItem() {
   const itemTitle = "صفحة من نحن";
   return (
     <AccordionItem title={itemTitle}>
-      <p>Test</p>
+      <div>
+        {/* Button  */}
+        <ButtonIcon
+          label="إضافة صورة من نحن"
+          icon={<RiGalleryLine />}
+          clickHandler={() => alert(2)}
+        />
+        {/* Image  */}
+
+        <div className="my-3 max-w-[250px] max-h-[250px] bg-red-300 overflow-hidden rounded-md">
+          <Image
+            src={"https://picsum.photos/300/300"}
+            height={160}
+            alt="Norway"
+            fit="contain"
+            fallbackSrc="https://placehold.co/600x400?text=Placeholder"
+          />
+        </div>
+      </div>
+      {/* section two  */}
+      <div>
+        {/* Button  */}
+        <ButtonIcon
+          label="إضافة صورة التعريف بالجامعة"
+          icon={<RiGalleryLine />}
+          clickHandler={() => alert(2)}
+        />
+        {/* Image  */}
+
+        <div className="my-3 max-w-[250px] max-h-[250px] bg-red-300 overflow-hidden rounded-md">
+          <Image
+            src={"https://picsum.photos/300/300"}
+            height={160}
+            alt="Norway"
+            fit="contain"
+            fallbackSrc="https://placehold.co/600x400?text=Placeholder"
+          />
+        </div>
+      </div>
     </AccordionItem>
   );
 }
