47ae493b7c373db778125a1473b290f19ab0dbc2
Moayed Alhagy <moayedlaravel@gmail.com>
2024-11-03 13:12:25 +0300

    General settings done !

--- src/components/PagesMangerItems/AboutUniItem.tsx
+++ src/components/PagesMangerItems/AboutUniItem.tsx
@@ -1,10 +1,31 @@
+import { RiGalleryLine } from "@remixicon/react";
 import AccordionItem from "../AccordionItem";
+import ButtonIcon from "../ButtonIcon";
+import { Image } from "@mantine/core";
 
 export default function AboutUniItem() {
   const itemTitle = "صفحة عن الجامعة";
   return (
     <AccordionItem title={itemTitle}>
-      <p>Test</p>
+      <div>
+        {/* Button  */}
+        <ButtonIcon
+          label="إضافة صورة عن الجامعة"
+          icon={<RiGalleryLine />}
+          clickHandler={() => alert(2)}
+        />
+        {/* Image  */}
+
+        <div className="my-3 max-w-[250px] max-h-[250px] bg-red-300 overflow-hidden rounded-md">
+          <Image
+            src={"https://picsum.photos/300/300"}
+            height={160}
+            alt="Norway"
+            fit="contain"
+            fallbackSrc="https://placehold.co/600x400?text=Placeholder"
+          />
+        </div>
+      </div>
     </AccordionItem>
   );
 }
--- src/components/PagesMangerItems/DecisionsItem.tsx
+++ src/components/PagesMangerItems/DecisionsItem.tsx
@@ -1,10 +1,31 @@
+import { RiGalleryLine } from "@remixicon/react";
 import AccordionItem from "../AccordionItem";
+import ButtonIcon from "../ButtonIcon";
+import { Image } from "@mantine/core";
 
 export default function DecisionsItem() {
   const itemTitle = "صفحة الأنظمة والقرارات";
   return (
     <AccordionItem title={itemTitle}>
-      <p>Test</p>
+      <div>
+        {/* Button  */}
+        <ButtonIcon
+          label="إضافة صورة الأنظمة والقرارات"
+          icon={<RiGalleryLine />}
+          clickHandler={() => alert(2)}
+        />
+        {/* Image  */}
+
+        <div className="my-3 max-w-[250px] max-h-[250px] bg-red-300 overflow-hidden rounded-md">
+          <Image
+            src={"https://picsum.photos/300/300"}
+            height={160}
+            alt="Norway"
+            fit="contain"
+            fallbackSrc="https://placehold.co/600x400?text=Placeholder"
+          />
+        </div>
+      </div>
     </AccordionItem>
   );
 }
